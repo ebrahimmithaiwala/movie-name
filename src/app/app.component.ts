35be6e2846fee95e79ea6a566412d1627485ca8f
Ebrahim <emithaiwala@gmail.com>
2020-07-27 19:30:52 +0530

    movie-playlist commited for the first time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+export interface Movie {
+  title: string;
+  src: string;
+  type: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-  title = 'movie-playlist';
+  playlist: Array<Movie> = [
+      {
+          title: 'movie-playlist',
+          src: 'https://valuefy-assignment-x.herokuapp.com/api/getVideos',
+          type: 'video/mp4'
+      }
+  ];
+
+  currentIndex = 0;
+    currentItem: Movie = this.playlist[ this.currentIndex ];
+
+    onClickPlaylistItem(item: Movie) {
+        this.currentIndex = index;
+        this.currentItem = item;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { VgCoreModule } from 'videogular2/compiled/core';
+import { VgControlsModule } from 'videogular2/compiled/controls';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+       AppComponent
+   ],
+   imports: [
+       BrowserModule,
+       AppRoutingModule,
+       VgCoreModule,
+       VgControlsModule
+   ],
+   providers: [],
+   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
